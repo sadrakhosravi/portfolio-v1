94de24a5367764153ba93b51429dbb36984adcf5
Sadra Khosravi <sadrakh@outlook.com>
2021-03-10 14:24:15 -0800

    Add projects to data.json file and formated index.pug template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+//Portfolio JSON Data
+const { projects } = require('../data.json');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.locals.projects = projects;
+  res.render('index');
 });
 
 module.exports = router;
