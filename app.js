35dccafa8b48a46d43971e2511758714f6c32eee
Sadra Khosravi <sadrakh@outlook.com>
2021-03-10 18:05:22 -0800

    Add 404 error handler and remove cookie-parser

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -12,7 +10,6 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //Routes
@@ -20,18 +17,16 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next();
+  const err = new Error('Not found!');
+  err.message = 'Sorry, the page was not found!';
+  err.status = 404;
+  console.log(`An error occured: ${err.status} - ${err.message}`);
+  next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.render('about');
 });
 
 module.exports = app;
